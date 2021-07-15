024c350051deec32b7da0fac3b9b68cf68a95b20
Alex Dutton <alex@cottagelabs.com>
2021-07-15 16:02:57 +0100

    Allow email to be configured by environment variable.

--- settings.py
+++ settings.py
@@ -15,3 +15,10 @@ MIDDLEWARE_CLASSES.insert(
     MIDDLEWARE_CLASSES.index("django.middleware.security.SecurityMiddleware") + 1,
     "whitenoise.middleware.WhiteNoiseMiddleware",
 )
+
+EMAIL_BACKEND = env.str('EMAIL_BACKEND', default=janeway_global_settings.EMAIL_BACKEND)
+EMAIL_HOST = env.str('EMAIL_HOST', default=janeway_global_settings.EMAIL_HOST)
+EMAIL_PORT = env.int('EMAIL_PORT', default=25)
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', default=janeway_global_settings.EMAIL_HOST_USER)
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', default=janeway_global_settings.EMAIL_HOST_PASSWORD)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=janeway_global_settings.EMAIL_USE_TLS)
